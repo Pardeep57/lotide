6ac346698922992cdcdc0fb52bfa05541f2aac1c
Pardeep57 <pardeepofficial57@gmail.com>
2022-08-24 00:27:19 +0000

    To get first elements

---
+++ head.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+ 
+ const assertEqual = function(actual, expected) {
+
+if(actual === expected){
+
+console.log( `Assertion Passed: ${actual} === ${expected}` );
+}
+
+else {
+console.log(`Assertion failed:${actual} !== ${expected}` );
+}
+
+};
+ 
+ function head(array) {
+
+let arrayhead = array[0];
+return arrayhead ;
+};
+
+
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+
