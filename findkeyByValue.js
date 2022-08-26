b098138349337375b25f2b6839d5170eb4c412cd
Pardeep57 <pardeepofficial57@gmail.com>
2022-08-26 09:21:13 +0000

    Find values

---
+++ findkeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (shows, showToFind) {
+  for (const keys of Object.keys(shows)) {
+    if (shows[keys] === showToFind) {
+      return keys;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
