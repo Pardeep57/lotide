b78ac269b6fab44450036b4f0cf9a284e049e20b
Pardeep57 <pardeepofficial57@gmail.com>
2022-08-30 04:50:16 +0000

    updatedArraysEquals

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,3 @@
-/*
-const assertArraysEqual = function(array1, array2){
-    for (let i = 0; i < array1.length; i++){
-        if (array1[i] !== array2[i]) {
-         console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-        }
-      }
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  }
-
-  const assertEqual = function(actual, expected) {
-
-    if(actual === expected){
-    
-    console.log( `Assertion Passed: ${actual} === ${expected}` );
-    }
-    
-    else {
-    console.log(`Assertion failed:${actual} !== ${expected}` );
-    }
-    
-    };
-    
-// TEST CODE
-// eqArrays([1, 2, 3], [1, 2, 3]); // => true
-// eqArrays([1, 2, 3], [3, 2, 1]); // => false
-// eqArrays(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true?
-
-*/
 
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(actual, expected) {
